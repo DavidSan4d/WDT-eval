7aa9644eaf0e6752b515d23665b13a5fb8670534
lt262337 <55994680+lt262337@users.noreply.github.com>
2022-07-15 13:37:12 -0600

    Create bin_Common.sh

---
+++ domain/bin/bin_Common.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+#    **********************************************************************    #
+#                                                                              #
+#    This file configures environment independent variables that are           #
+#    imported automatically from the shell scripts.                            #
+#                                                                              #
+#    **********************************************************************    #
+
+#JRE_LINK=/opt/fedex/cis/jre
+#WLS_LINK=/opt/fedex/cis/wls
+#PROJECT_DOMAIN_HOME=/opt/fedex/cisapp/domain/current/CisDomain
+#PROJECT_JAR_HOME=/opt/fedex/cisapp/domain/current/lib
+#BIN_PROJECT_HOME=/opt/fedex/cisapp/domain/current/bin
+#VAR_PROJECT_HOME=/var/fedex/cisapp
+#DEPLOYMENT_PROJECT_TARGET=CisCluster
+#PROJECT_LOG_HOME=/var/fedex/cisapp/log
+EAINUMBER=3538245
